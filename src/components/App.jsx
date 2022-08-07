fc1a608e96b01cdd9a4789638b820abdf85eb6f0
Serhii Kushneruk <svkushneruk@ukr.net>
2022-08-07 23:02:03 +0300

    add default contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,19 @@ import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import css from 'components/App.module.css';
 
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+    const localStorageTarget = JSON.parse(
+      window.localStorage.getItem('contacts')
+    );
+    return localStorageTarget.length > 0 ? localStorageTarget : defaultContacts;
   });
   const [filter, setFilter] = useState('');
 
